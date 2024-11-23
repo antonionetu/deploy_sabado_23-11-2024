0979b53a02d67112b155f64332615f947fb9c815
Antônio Fernandes de Santana Neto <1231108140@acd.grupotiradentes.com>
2024-11-23 09:21:10 -0300

    upload

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meuprojeto.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projeto_deploy.settings")
 app = get_wsgi_application()
